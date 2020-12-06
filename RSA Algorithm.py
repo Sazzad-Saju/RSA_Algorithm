34c590d2b452e39c0dbf4b47eb8223a729c404bf
Sazzad-Saju <sazzadsaju17@gmail.com>
2020-12-07 03:24:37 +0600

    Output-DEC

--- RSA Algorithm.py
+++ RSA Algorithm.py
@@ -1,63 +1,107 @@
-import sys, threading
+def ConvertToInt(message):
+    grd = 1
+    num = 0
+    message = message[::-1]
+    for i in range(0, len(message), +1):
+        num = num + ord(message[i]) * grd
+        grd *= 256
+    return num
 
-sys.setrecursionlimit(10**7)
-threading.stack_size(2**27)
-
-def ConvertToInt(message_str):
-    res = 0
-    for i in range(len(message_str)):
-        res = res*256 + ord(message_str[i])
-    return res
-
-def ConvertToStr(n):
-    res = ""
-    while n>0:
-        res += chr(n%256)
-        n //= 256
-    return res[::-1]
-
-def PowMod(a,n,mod):
-    if n==0:
-        return 1 % mod
-    elif n==1:
-        return a % mod
-    else:
-        b = PowMod(a, n//2, mod)
-        b = b * b % mod
-        if n%2 == 0:
-            return b
-        else:
-            return b * a % mod
-
-def ExtendedEuclid(a,b):
-    if b == 0:
-        return (1,0)
-    (x,y) = ExtendedEuclid(b, a%b)
-    k = a // b
-    return (y,x- k * y)
-
-def InvertModulo(a,n):
-    (b,x) = ExtendedEuclid(a,n)
-    if b < 0:
-        b = (b % n + n) % n
+def mod_ex(b, k, m):
+    i = 1
+    j = 0
+    while (j <= k):
+        b = (b * i) % m
+        i = b
+        j += 1
     return b
 
+def PowMod(b, e, m):
+    bin_e = bin(e)
+    bin_e = bin_e[::-1]
+    ln = len(bin_e)
+    result = 1
+    for i in range(0, ln - 2, +1):
+        if (bin_e[i] == '1'):
+            result *= mod_ex(b, i, m)
+    return result % m
+
 def Encrypt(message, modulo, exponent):
-    s = ConvertToInt(message)
-    return PowMod(s,exponent, modulo)
+    cytxt = ""
+    w = modulo // 256
+    letter = 0
+    while (w != 0):
+        w = w // 256
+        letter += 1
+    cyln = pow(256, letter)
+    cyln = len(str(cyln)) + 1
+    s = message
+    for i in range(0, len(message), +letter):
+        s1 = s[:letter]
+        Con = ConvertToInt(s1)
+        cytxt1 = str(PowMod(Con, exponent, modulo))
+        if (len(cytxt1) < cyln):
+            while ((cyln - len(cytxt1) > 0)):
+                zr = "0"
+                cytxt1 = zr + cytxt1
+            cytxt += cytxt1
+        else:
+            cytxt += cytxt1
+        s = s[letter:]
+    return cytxt
+
+def eea(a, b):
+    if (a % b == 0):
+        return (b, 0, 1)
+    else:
+        gcd, s, t = eea(b, a % b)
+        s = s - ((a // b) * t)
+        return (gcd, t, s)
+
+def InvertModulo(e, r):
+    gcd, s, _ = eea(e, r)
+    if (gcd != 1):
+        return None
+    else:
+        return s % r
+
+def ConvertToStr(m):
+    st = ""
+    while (m != 0):
+        temp = m % 256
+        st += chr(temp)
+        chr(temp)
+        m = m - temp
+        m = m // 256
+    st = st[::-1]
+    return st
 
 def Decrypt(ciphertext, p, q, exponent):
-    n = (p-1) * (q-1)
+    modulo = p * q
+    n = (p - 1) * (q - 1)
     d = InvertModulo(exponent, n)
-    return ConvertToStr(PowMod(ciphertext,d,p * q ))
+    w = modulo // 256
+    letter = 0
+    while (w != 0):
+        w = w // 256
+        letter += 1
+    cyln = pow(256, letter)
+    cyln = len(str(cyln)) + 1
+    s = ciphertext
+    m = ""
+    for i in range(0, len(ciphertext), +cyln):
+        s1 = s[:cyln]
+        Con = int(s1)
+        m += ConvertToStr(PowMod(Con, d, modulo))
+        s = s[cyln:]
+    return m
 
-p = 779849711281
-q = 748173698927
-e = 1018651
 
-n = p*q
-message = "Down the r"
-ciphertext = Encrypt(message, n, e)
+p = 57704576143051
+q = 838744063
+e = 2237
+modulo = p * q
+ciphertext = Encrypt("Down the Rabbit-Hole", modulo, e)
 print(ciphertext)
-message = Decrypt(ciphertext,p,q,e)
+message = Decrypt(ciphertext, p, q, e)
 print(message)
